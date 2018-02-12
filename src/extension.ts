4997ae10cb740fca4d1e44c39f73c44fb3d23323
hotchemi <hot.chemistry21@gmail.com>
2018-02-13 06:34:53 +0900

    Add command to menu and fix to open url with browser.

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,23 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { open } from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-find-pr" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        openUrl('https://www.google.com')
     });
 
     context.subscriptions.push(disposable);
+}
+
+function openUrl(url: string) {
+    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url))
 }
 
 // this method is called when your extension is deactivated
