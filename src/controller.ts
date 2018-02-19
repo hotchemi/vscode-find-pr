f8574984feb0afba1f025d3c6445b1bfd240f56f
hotchemi <hot.chemistry21@gmail.com>
2018-02-20 03:50:12 +0900

    Extract method to controler.

--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,8 @@
 import { window } from "vscode";
 import Path = require("path");
-import { SpawnSyncOptions } from "child_process";
+import { SpawnSyncOptions, spawn } from "child_process";
 
-export class GitBlameController {
+export class Controller {
   activeFileName(): string | undefined {
     return window.activeTextEditor && window.activeTextEditor.document.fileName;
   }
@@ -23,4 +23,27 @@ export class GitBlameController {
       cwd: this.activeDirName()
     };
   }
+
+  async execute() {
+    const filename = this.activeFileName();
+    const line = this.activeLineNumber();
+    const currentDir = this.activeDirName();
+    const options = {
+      cwd: currentDir
+    };
+
+    const blame = spawn(
+      "git",
+      ["blame", "--first-parent", "-L", `${line},${line}`, filename],
+      options
+    );
+    const readline = require("readline");
+    const lines = readline.createInterface({ input: blame.stdout });
+    lines.on("line", input => {
+      const [hash, content] = input.split(/ .*?\) /);
+      console.log(hash);
+    });
+  }
+
+  dispose(): void {}
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,14 @@
-import * as vscode from "vscode";
-import { open } from "fs";
-import { spawn } from "child_process";
-import child_process = require("child_process");
-import { GIT_COMMAND_IN_PATH } from "./constants";
-const readline = require("readline");
-import Path = require("path");
-import { getPullRequestNumber, blame } from "./git/history";
+import { commands, ExtensionContext } from "vscode";
+import { Controller } from "./controller";
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-    const filename = vscode.window.activeTextEditor.document.fileName;
-    const line = vscode.window.activeTextEditor.selection.active.line;
-    const currentDirectory = Path.dirname(filename);
-    const options = {
-      cwd: currentDirectory
-    };
-
-    const blame = spawn(
-      "git",
-      ["blame", "--first-parent", "-L", `${line},${line}`, filename],
-      options
-    );
-    const readline = require("readline");
-    const lines = readline.createInterface({ input: blame.stdout });
-    lines.on("line", input => {
-      const [hash, content] = input.split(/ .*?\) /);
-      console.log(hash);
-    });
-  });
-
-  context.subscriptions.push(disposable);
+export function activate(context: ExtensionContext) {
+  const controller = new Controller();
+  const disposable = commands.registerCommand(
+    "extension.sayHello",
+    controller.execute,
+    controller
+  );
+  context.subscriptions.push(controller, disposable);
 }
 
 export function deactivate() {}
